1e08c838b65d8eabaca63f8c2153037b7634943b
Tanish Kalwad <kalwad@umich.edu>
2025-04-30 22:05:48 -0400

    bind to port

--- backend/app.py
+++ backend/app.py
@@ -17,5 +17,9 @@ def trace():
         return jsonify({"error": str(e)}), 400
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # default if not set
+    app.run(host="0.0.0.0", port=port, debug=True)
+
